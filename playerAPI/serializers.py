8811a15d5c2ca9db4ac104b2b3adfc0a65e2c5bf
kacperadach <kacperadach@gmail.com>
2016-01-31 22:53:35 -0500

    fixed serializer for week performance data

--- playerAPI/serializers.py
+++ playerAPI/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from models import Player
+from models import Player, WeekPerformance
 
+
+class WeekPerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WeekPerformance
+        fields = ('week', 'points')
 
 class PlayerSerializer(serializers.ModelSerializer):
-    WeekPerformances = serializers.StringRelatedField(many=True)
+    WeekPerformances = WeekPerformanceSerializer(many=True, read_only=True)
 
     class Meta:
         model = Player
